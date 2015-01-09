a8c48069c1e1cdf959cd7a4f88ef826781cdfec9
Manuel Ayaso <manuel.ayaso.enriquez@gmail.com>
2015-01-09 09:09:31 +0000

    Change general script name to loadData

--- loadData.R
+++ loadData.R
@@ -1,5 +1,5 @@
-getData <- function (dir = "./", file) {
-    wholeData <- read.csv(paste(dir, file, sep = ""), header = TRUE, sep = ";")
+getData <- function (dir = "..", file = "household_power_consumption.txt") {
+    wholeData <- read.csv(paste(dir, "/", file, sep = ""), header = TRUE, sep = ";")
     startDate <- as.Date("2007-02-01", format = "%Y-%m-%d")
     endDate <- as.Date("2007-02-02", format = "%Y-%m-%d")
     data <- wholeData[as.Date(wholeData$Date, format = "%d/%m/%Y") >= startDate & 
