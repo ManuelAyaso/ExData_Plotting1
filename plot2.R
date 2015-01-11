6cb97f569dcb1e3176110ae0f3661216e3a65c71
Manuel Ayaso <manuel.ayaso.enriquez@gmail.com>
2015-01-11 14:44:15 +0000

    Added plot 3 script

--- plot2.R
+++ plot2.R
@@ -2,5 +2,5 @@ source("loadData.R")
 
 data <- getData()
 
-plot(data$Timestamp, data$Global_active_power, type = "l", 
+plot(data$Timestamp, data$Global_active_power, type = "n", 
      xlab = "", ylab = "Global Active Power (kilowatts)")
--- plot3.R
+++ plot3.R
@@ -1,3 +1,10 @@
 source("loadData.R")
 
 data <- getData()
+
+plot(data$Timestamp, data$Sub_metering_1, type = "l", 
+     xlab = "", ylab = "Energy sub metering")
+lines(data$Timestamp, data$Sub_metering_2, col = "red")
+lines(data$Timestamp, data$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), lty = c(1, 1, 1), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
